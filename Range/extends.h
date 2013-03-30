9cb13638a9894f012e68dc6ce54f34e8a87f0758
pandaman64 <kointosudesuyo@infoseek.jp>
2013-03-30 16:22:35 +0900

    Change Range.Extends, implemention of Algorithm.Map
    Change interface of Range.Extends.
    Change implemention of Alorithm.Map with using Range.Extends.

--- Range/extends.h
+++ Range/extends.h
@@ -1,33 +1,35 @@
 #ifndef PANLIB_RANGE_EXTENDS
 #define PANLIB_RANGE_EXTENDS
 
+#include "range_value_type.h"
 #include "concept.h"
 
 namespace panlib{
 namespace range{
 
 template<typename Base,typename Derived>
-struct extends : concept::choose_range_concept<Base,extends<Base,Derived>>{
-private:
+struct extends : concept::choose_range_concept<Base,Derived>{
+	using base_type = concept::choose_range_concept<Base,Derived>;
+protected:
 	Base range_base;
 	
-public
+public:
+	using value_type = typename std::remove_reference<typename range_value_type<typename std::remove_reference<Base>::type>::type>::type;
+
 	extends(Base base) : range_base(std::move(base)){
 	}
 
-	template<typename D = Derived>
 	auto front_()
-	->decltype(util::crtp_cast<D>(*this).transform(range_base.front())){
-		return util::crtp_cast<D>(*this).transform(range_base.front());
+	->decltype(range_base.front()){
+		return range_base.front();
 	}
 	void pop_front_(){
 		range_base.pop_front();
 	}
 
-	template<typename D = Derived>
 	auto back_()
-	->decltype(util::crtp_cast<D>(*this).transform(range_base.back())){
-		return util::crtp_cast<D>(*this).transform(range_base.back());
+	->decltype(range_base.front()){
+		return range_base.front();
 	}
 	void pop_back_(){
 		range_base.pop_back();
